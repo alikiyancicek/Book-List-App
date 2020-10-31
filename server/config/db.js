a4d6c9290c15204c5a70b26fd47940695a5933d9
alikiyancicek <ali.kiyancicek@gmail.com>
2020-10-30 22:06:22 -0400

    first mongo update

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://alikiyancicek:Ali123.123@midtermproject.8afev.mongodb.net/books229?retryWrites=true&w=majority"
 };
